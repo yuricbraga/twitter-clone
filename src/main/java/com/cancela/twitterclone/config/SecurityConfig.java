12402ce4fb2a58119171f91f244fe1feb47ac515
Yuri Braga <yuricancelabraga@gmail.com>
2021-03-18 12:20:07 -0300

    Initial commit

---
+++ src/main/java/com/cancela/twitterclone/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.cancela.twitterclone.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
---
+++ src/test/java/com/cancela/twitterclone/TwittercloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancela.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittercloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
